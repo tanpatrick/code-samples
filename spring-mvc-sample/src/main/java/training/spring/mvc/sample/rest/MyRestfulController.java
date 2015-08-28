e020993266f50e5c4e7e6560c975fb64c4bcee4f
Joseph Patrick Tan <Joseph Patrick Tan@LT1504196>
2015-08-28 10:40:19 +0800

    - added parent/child controller

--- spring-mvc-sample/src/main/java/training/spring/mvc/sample/rest/MyRestfulController.java
+++ spring-mvc-sample/src/main/java/training/spring/mvc/sample/rest/MyRestfulController.java
@@ -3,6 +3,7 @@ package training.spring.mvc.sample.rest;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import training.spring.mvc.sample.model.Child;
 import training.spring.mvc.sample.model.Parent;
 
 /**
@@ -21,5 +22,16 @@ public class MyRestfulController {
     @RequestMapping(value = "parent", produces = {MediaType.APPLICATION_JSON_VALUE})
     public Parent parent() {
         return new Parent(1, "Patrick");
+    }
+
+    @RequestMapping(value = "parent/children", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Parent parentChildren() {
+        Parent parent = new Parent(1, "Patrick");
+
+        for (int i = 1; i <= 2; i++) {
+            parent.getChildren().add(new Child(i, "child [" + i + "]", parent));
+        }
+
+        return parent;
     }
 }
