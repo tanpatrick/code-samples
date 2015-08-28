6080a22df09e19bb9658b10d8e2afe9b9cc64e2f
Joseph Patrick Tan <Joseph Patrick Tan@LT1504196>
2015-08-28 09:10:04 +0800

    - added parent-child model

---
+++ spring-mvc-sample/src/main/java/training/spring/mvc/sample/model/Child.java
@@ -0,0 +1,37 @@
+package training.spring.mvc.sample.model;
+
+/**
+ *
+ * @author Patrick Tan
+ */
+public class Child {
+
+    private Integer childId;
+    private String childName;
+    private Parent parent;
+
+    public Integer getChildId() {
+        return childId;
+    }
+
+    public void setChildId(Integer childId) {
+        this.childId = childId;
+    }
+
+    public String getChildName() {
+        return childName;
+    }
+
+    public void setChildName(String childName) {
+        this.childName = childName;
+    }
+
+    public Parent getParent() {
+        return parent;
+    }
+
+    public void setParent(Parent parent) {
+        this.parent = parent;
+    }
+
+}
---
+++ spring-mvc-sample/src/main/java/training/spring/mvc/sample/model/Parent.java
@@ -0,0 +1,45 @@
+package training.spring.mvc.sample.model;
+
+import java.util.List;
+
+/**
+ *
+ * @author Patrick Tan
+ */
+public class Parent {
+
+    private Integer parentId;
+    private String parentName;
+    private List<Child> children;
+
+    public Parent(Integer parentId, String parentName) {
+        this.parentId = parentId;
+        this.parentName = parentName;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getParentName() {
+        return parentName;
+    }
+
+    public void setParentName(String parentName) {
+        this.parentName = parentName;
+    }
+
+    public List<Child> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<Child> children) {
+        this.children = children;
+    }
+
+   
+}
